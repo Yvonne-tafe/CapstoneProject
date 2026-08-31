66b182e8c29a484c8a9c7276a9c08e917a3d4c8a
Yvonne-tafe <30138956@tafe.wa.edu.au>
2026-08-31 17:50:27 +0800

    Create Template_details.php
    template for detail page

---
+++ template(html + CSS)/Template_details.php
@@ -0,0 +1,11 @@
+<?php
+require_once __DIR__ . '/layout.php';
+showHeader('Services', 'services');
+?>
+
+<section class="form-section">
+    <h1>Services</h1>
+    <p>Content</p>
+</section>
+
+<?php showFooter(); ?>
